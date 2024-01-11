68193acd74396f753a9c320d4608edfb7d8be77d
Austin <austin@austinwarnock.tech>
2024-01-11 14:26:06 +0000

    Completed implementation of awhead

---
+++ awhead.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[]){
+
+    FILE *fp;
+    int opt;
+    int line_count = 10;
+
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch (opt) {
+            case 'n':
+                line_count = atoi(optarg);
+                break;
+            case '?':
+                fprintf(stderr, "Unknown option: -%c\n", optopt);
+                return EXIT_FAILURE;
+        }
+    }
+    
+    fp = fopen(argv[optind], "rb");
+    if (fp == NULL){
+        fprintf(stderr, "No such file or directory %s\n", argv[optind]);
+        exit(-1);
+    }
+
+
+    char * line = NULL;
+    size_t count = 0;
+    size_t len = 0;
+    while ((getline(&line, &len, fp)) != -1 && count < line_count) {
+        printf("%s", line);
+        count++;
+    }
+
+    return 1;
+    
+}
